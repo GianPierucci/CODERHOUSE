7d76bdf683a409c3b7c606012dee0ec4d7e718fe
Gianfranco Pierucci <gian.pieru@hotmail.com>
2022-09-13 11:35:28 -0300

    Javascript

--- js/formulario.js
+++ js/formulario.js
@@ -1,58 +1,40 @@
 let btnRegistrarse = document.getElementById("boton-form")
-let emailLogin = document.getElementById("form-email")
-let nombreLogin = document.getElementById("form-nombre")
-let passLogin = document.getElementById("form-pass")
 let recordar = document.getElementById("btn-recordar")
 
 
-let usuariosDB = []
+function guardarDatosForm(storage) {
 
-function guardarDatosForm(usuarioS, storage) {
+    let name = document.getElementById("form-nombre").value;
+    let user = document.getElementById("form-email").value;
+    let pass = document.getElementById("form-pass").value;
 
     const usuario = {
-        "name": usuarioS.nombre,
-        "user": usuarioS.mail,
-        "pass": usuarioS.contraseña
+        "nombre": name,
+        "user": user,
+        "pass": pass
     }
 
-    storage.setItem("usuario", JSON.stringify(usuario));
-}
-
-function recuperarDatosUs(storage) {
-    return JSON.parse( storage.getItem("usuario") )
-}
-
-function validarUsuario (usuarsDB, user, pass){
-    let encontrado = usuarsDB.find((usuario)=> user.mail == user)
-
-    if (typeof encontrado === "undefined"){
-        return false;}
-    else{
-        if (encontrado.pass != pass){
-            return false;}
-        else{
-            return encontrado;
-        }
+    if(storage === "localStorage"){
+        localStorage.setItem("user", JSON.stringify(usuario))
+    }
+    if(storage === "sessionStorage"){
+        sessionStorage.setItem("user", JSON.stringify(usuario))
     }
 }
 
 
-btnRegistrarse.addEventListener("click", (e)=>{
+btnRegistrarse.addEventListener("click",(e)=>{
     e.preventDefault();
-    if(!emailLogin.value || !passLogin.value){
-        alert("campos requeridos")
-    }else{
-        let data = validarUsuario(usuariosDB, emailLogin, passLogin)
-
-        if(!data){
-            alert("usuario no encontrado")
-        }else{
-            if(recordar.checked){
-                guardarDatosForm(data, localStorage)
-            }else{
-                guardarDatosForm(data, sessionStorage)
-            }
-        }
-        alert("te logueaste con exito")
+    if(recordar.checked){
+        guardarDatosForm("localStorage")
+        
+    }
+    else{
+        guardarDatosForm("sessionStorage")
     }
 })
+
+
+
+
+
--- js/script2.js
+++ js/script2.js
@@ -1,28 +1,16 @@
 let carrito = []
 
-const stockProds = [
-    { id: 1, img: "../imgs/cuchillo1.jpg", cantidad: 1, producto: "Cuchillo", tipo: "Cocina", marca: "Mundial", largo: "25cm", precio: 2000 },
-    { id: 2, img: "../imgs/cuchillo1.jpg", cantidad: 1, producto: "Cuchillo", tipo: "Cocina", marca: "Mundial", largo: "30cm", precio: 2500 },
-    { id: 3, img: "../imgs/cuchillo1.jpg", cantidad: 1, producto: "Cuchillo", tipo: "Cocina", marca: "Mundial", largo: "35cm", precio: 3000 },
-    { id: 4, img: "../imgs/cuchillo1.jpg", cantidad: 1, producto: "Cuchillo", tipo: "Cocina", marca: "Mundial", largo: "40cm", precio: 3500 },
-    { id: 5, img: "../imgs/tijera-mdentado.jpg", cantidad: 1, producto: "Tijera", tipo: "Hogar", marca: "Mundial", largo: "15cm", precio: 1500 },
-    { id: 6, img: "../imgs/tijera-mdentado.jpg", cantidad: 1, producto: "Tijera", tipo: "Hogar", marca: "Mundial", largo: "15cm", precio: 1800 },
-    { id: 7, img: "../imgs/tijera-mdentado.jpg", cantidad: 1, producto: "Tijera", tipo: "Hogar", marca: "Mundial", largo: "15cm", precio: 1500 },
-    { id: 8, img: "../imgs/tijera-mdentado.jpg", cantidad: 1, producto: "Tijera", tipo: "Hogar", marca: "Mundial", largo: "15cm", precio: 1800 },
-    { id: 9, img: "../imgs/chaira-mundial.jpg", cantidad: 1, producto: "Chaira", tipo: "Hogar", marca: "Mundial", largo: "20cm", precio: 1500 },
-    { id: 10, img: "../imgs/chaira-mundial.jpg", cantidad: 1, producto: "Chaira", tipo: "Hogar", marca: "Mundial", largo: "20cm", precio: 1500 },
-    { id: 11, img: "../imgs/chaira-mundial.jpg", cantidad: 1, producto: "Chaira", tipo: "Hogar", marca: "Mundial", largo: "20cm", precio: 1500 },
-    { id: 12, img: "../imgs/chaira-mundial.jpg", cantidad: 1, producto: "Chaira", tipo: "Hogar", marca: "Mundial", largo: "20cm", precio: 1500 },
-]
 
+const fetchData = async () => {
+    try {
+        const resp = await fetch("../js/data.json")
+        const data = await resp.json();
 
-
-const mostrarProd = (productos) => {
-    const divProds = document.getElementById("secc-prods")
-    productos.forEach(producto => {
-        const div = document.createElement("div")
-        div.classList.add("caja-prods")
-        div.innerHTML = `<img class="img-cuchillo" src="${producto.img}"
+        const divProds = document.getElementById("secc-prods")
+        data.forEach(producto => {
+            const div = document.createElement("div")
+            div.classList.add("caja-prods")
+            div.innerHTML = `<img class="img-cuchillo" src="${producto.img}"
                         <div class="info-prods">
                             <h5 class="nomb-prod">${producto.producto}
                                 ${producto.tipo}
@@ -35,104 +23,83 @@ const mostrarProd = (productos) => {
                                 <button type="button" id="boton${producto.id}" class="btn btn-danger">Comprar</button>
                             </div>
                         </div>`
-        divProds.appendChild(div);
-        const boton = document.getElementById(`boton${producto.id}`)
-        boton.addEventListener("click", () => {
-            carritoIndex(producto.id)
-            Toastify({
-                text: "El producto se agregó a tu carro de compras",
-                duration: 3000,
-                gravity: "bottom",
-                position: "right",
-                style: {
-                    background: "#dc3545",
-                },
-            }).showToast();
+            divProds.appendChild(div);
+            const boton = document.getElementById(`boton${producto.id}`)
+            boton.addEventListener("click", () => {
+                carritoIndex(producto.id)
+                Toastify({
+                    text: "El producto se agregó a tu carro de compras",
+                    duration: 3000,
+                    gravity: "bottom",
+                    position: "right",
+                    style: {
+                        background: "#dc3545",
+                    },
+                }).showToast();
+            })
         })
-    })
-}
-mostrarProd(stockProds)
+        const carritoIndex = (productoId) => {
+            const cajaCarrito = document.getElementById("div-carrito");
 
-
-
-const carritoIndex = (productoId) => {
-    const cajaCarrito = document.getElementById("div-carrito");
-
-    const agregarAlCarr = () => {
-        const producto = stockProds.find(producto => producto.id === productoId);
-        carrito.push(producto)
-        const div = document.createElement("div")
-        div.classList.add("productosEnCarr")
-        div.innerHTML = `<p class="prodsCrt-style">${producto.cantidad}</p>
-                        <p class="prodsCrt-style">${producto.producto}
+            const agregarAlCarr = () => {
+                const producto = data.find(producto => producto.id === productoId);
+                carrito.push(producto)
+                const div = document.createElement("div")
+                div.classList.add("productosEnCarr")
+                div.innerHTML = `<p class="cantidadProds">${producto.cantidad}</p>
+                        <p>${producto.producto}
                             ${producto.tipo}
                             ${producto.marca}</p>
-                        <p class="prodsCrt-style">${producto.precio}</p>
+                        <p class="precioProd">$${producto.precio}</p>
                         <button type="button" class="boton-sacar"><i class="fa-regular fa-trash-can"></i></button>
                         `
-        cajaCarrito.appendChild(div)
-        div.querySelector(".boton-sacar").addEventListener("click", eliminarProdCarr)
-    }
-    agregarAlCarr();
-
-
-}
-
-function eliminarProdCarr(e) {
-    const btnClicked = e.target;
-
-    Swal.fire({
-        icon: "warning",
-        title: 'Eliminar producto',
-        text: "¿Estas seguro de eliminar el producto?",
-        showCancelButton: true,
-        confirmButtonText: 'Eliminar',
-        cancelButtonText: "Cancelar"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            btnClicked.closest(".productosEnCarr").remove()
-            Toastify({
-                text: "Eliminaste el producto",
-                duration: 2500,
-                gravity: "bottom",
-                position: "right",
-                style: {
-                    background: "#dc3545",
-                },
-            }).showToast();
+                cajaCarrito.appendChild(div)
+                div.querySelector(".boton-sacar").addEventListener("click", eliminarProdCarr)
+            }
+            agregarAlCarr();
+            const precioTotal = document.querySelector(".precioTotalProds")
+            precioTotal.innerHTML = carrito.reduce((acc, producto) => acc + producto.precio, 0)
         }
-    })
-}
 
 
+        function eliminarProdCarr(e) {
+            const btnClicked = e.target;
 
-let usuariosDB = []
-
-function guardarDatosForm(usuarioS, storage) {
-
-    const usuario = {
-        "name": usuarioS.nombre,
-        "user": usuarioS.mail,
-        "pass": usuarioS.contraseña
-    }
-
-    storage.setItem("usuario", JSON.stringify(usuario));
-}
-
-function recuperarDatosUs(storage) {
-    return JSON.parse( storage.getItem("usuario") )
-}
-
-function validarUsuario (usuariosDB, user, pass){
-    let encontrado = usuariosDB.find((usuario)=> user.mail == user)
-
-    if (typeof encontrado === "undefined"){
-        return false;}
-    else{
-        if (encontrado.pass != pass){
-            return false;}
-        else{
-            return encontrado;
+            Swal.fire({
+                icon: "warning",
+                title: 'Eliminar producto',
+                text: "¿Estas seguro de eliminar el producto?",
+                showCancelButton: true,
+                confirmButtonText: 'Eliminar',
+                cancelButtonText: "Cancelar"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    btnClicked.closest(".productosEnCarr").remove()
+                    Toastify({
+                        text: "Eliminaste el producto",
+                        duration: 2500,
+                        gravity: "bottom",
+                        position: "right",
+                        style: {
+                            background: "#dc3545",
+                        },
+                    }).showToast();
+                }
+            })
         }
+    } catch (error) {
+        console.log(error)
     }
 }
+fetchData()
+
+
+
+
+
+
+
+
+
+
+
